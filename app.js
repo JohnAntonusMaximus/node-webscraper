6d33d0f7be1f06c54009e5ba5390f63b7e12cf56
JohnAntonusMaximus <johnradosta@gmail.com>
2016-06-20 17:43:53 -0500

    Changed env port for production

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
+var port = process.env.PORT || '3000' ;
 
-app.get('/scrape', function(req,res){
+app.get('/scrape', function(req , res){
     url= 'http://www.imdb.com/title/tt0364725/';
-    request(url, function (error, res, html){
+    request(url, function (error, response, html){
         if(!error){
             var $ = cheerio.load(html);
             
@@ -19,23 +20,33 @@ app.get('/scrape', function(req,res){
                 title = data.text();
                 release = data.children().first().text();
                 
-                json.title = title;
+                json.title = title.trim();
                 json.release = release;
-            });
+            })
             
             $('.subtext').filter(function(){
                 var data = $(this);
                 
                 rating = data.text();
                 ratingParsed =rating.substring(21,26);
-                json.rating = ratingParsed;
-            });
+                json.rating = ratingParsed.trim();
+            })
             
-            console.log(json);
-        }
-    });
-});
+            
+        } //if(!err)
+      
+            fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+           
+                console.log('File successfully written! - Check your project directory for the output.json file');
+                
+            }) // fs.writeFile()
+               
+                res.send('Check your console!')
+            
+        });  // request()
+}) // app.get()
 
-app.listen('8081');
+app.listen(port);
 console.log('Let the magic begin...');
 exports = module.exports = app;
+
